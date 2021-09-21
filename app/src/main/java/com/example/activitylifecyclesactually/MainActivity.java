f0edd7ac346c67dd2843258637d263c94a3283a2
archow <abedur_chowdhury@outlook.com>
2021-09-21 10:45:26 -0400

    - updated the saving and restoring of data

--- app/src/main/java/com/example/activitylifecyclesactually/MainActivity.java
+++ app/src/main/java/com/example/activitylifecyclesactually/MainActivity.java
@@ -1,31 +1,68 @@
 package com.example.activitylifecyclesactually;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.activitylifecyclesactually.model.User;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String USER_OBJECT = "USER_OBJECT";
+    private static final String COUNTER_VALUE = "COUNTER_VALUE";
     private static final String MAIN_ACTIVITY_LIFECYCLE = "MAIN_ACTIVITY_LIFECYCLE";
     private TextView mStartNextActivity;
+    private Button mCounterBtn;
+    private int value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(MAIN_ACTIVITY_LIFECYCLE, "on create called for Main activity");
+        value = 1;
+
+        mCounterBtn = findViewById(R.id.counter_btn);
 
         mStartNextActivity = findViewById(R.id.start_next_activity);
+        mStartNextActivity.setText(value + "");
         mStartNextActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Let's say I have a user object and I want to pass it to my intent
+                User user = new User("Will", "Smith");
+
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra(USER_OBJECT, user);
                 startActivity(intent);
             }
         });
+
+        mCounterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                value++;
+                mStartNextActivity.setText(value + "");
+            }
+        });
+
+        //restore data if bundle exists
+        if (savedInstanceState != null) {
+            value = savedInstanceState.getInt(COUNTER_VALUE);
+            mStartNextActivity.setText(value + "");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //save data using the Bundle provided
+        outState.putInt(COUNTER_VALUE, value);
     }
 
     @Override
---
+++ app/src/main/java/com/example/activitylifecyclesactually/model/User.java
@@ -0,0 +1,81 @@
+package com.example.activitylifecyclesactually.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.Objects;
+
+public class User implements Parcelable {
+    private String firstName;
+    private String lastName;
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    protected User(Parcel in) {
+        firstName = in.readString();
+        lastName = in.readString();
+    }
+
+    public static final Creator<User> CREATOR = new Creator<User>() {
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getFirstName(), user.getFirstName()) && Objects.equals(getLastName(), user.getLastName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getFirstName(), getLastName());
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+    }
+}
